bdb03045e33ebebd9fb2a1698510be51265da7f2
Tiago Boer Casagrande <tiago.boer@hotmail.com>
2020-10-06 17:37:05 -0300

    Updating value only if needed

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
-import React, {useContext} from 'react';
-import {useWindowDimensions} from 'react-native';
-import {NAMEDSTYLES, STATE} from './types';
+import React, {useContext, useEffect, useState} from 'react';
+import {useWindowDimensions, ImageStyle, TextStyle, ViewStyle} from 'react-native';
 import blacklist from './blacklist';
 
+type STATE = {
+  re: number;
+  width?: number;
+  height?: number;
+  orientation?: 'portrait' | 'landscape';
+};
+
+type NAMEDSTYLES<T> = {[P in keyof T]: ViewStyle | TextStyle | ImageStyle};
+
+const copy = (obj) => JSON.parse(JSON.stringify(obj));
 const checkBlackList = (key: string) => !blacklist.some((value) => key === value);
 
 const defaultValue: STATE = {re: 1};
@@ -21,22 +30,29 @@ export const useResponsive = <T extends NAMEDSTYLES<T> | NAMEDSTYLES<any>>(
   styles?: T | NAMEDSTYLES<T>,
 ): STATE & {styles?: T | NAMEDSTYLES<T>} => {
   const state = useContext(Context);
+  const [parsedStyle, setParsedStyle] = useState(styles);
 
-  const s = JSON.parse(JSON.stringify(styles));
+  useEffect(() => {
+    const apply_resposive = () => {
+      const temp = copy(styles);
 
-  if (!!styles) {
-    Object.entries(styles).forEach(([key, value]) => {
-      Object.entries(value).forEach(([subKey, subValue]) => {
-        if (typeof subValue === 'number' && checkBlackList(subKey)) {
-          s[key][subKey] = subValue * state.re;
-        } else if (typeof subValue === 'string' && subValue.startsWith('-')) {
-          s[key][subKey] = +subValue.substring(1);
-        } else if (typeof subValue === 'string' && subValue.startsWith('+')) {
-          s[key][subKey] = +subValue.substring(1) * state.re;
-        }
+      Object.entries(styles).forEach(([key, value]) => {
+        Object.entries(value).forEach(([subKey, subValue]) => {
+          if (typeof subValue === 'number' && checkBlackList(subKey)) {
+            temp[key][subKey] = subValue * state.re;
+          } else if (typeof subValue === 'string' && subValue.startsWith('-')) {
+            temp[key][subKey] = +subValue.substring(1);
+          } else if (typeof subValue === 'string' && subValue.startsWith('+')) {
+            temp[key][subKey] = +subValue.substring(1) * state.re;
+          }
+        });
       });
-    });
-  }
 
-  return {...state, styles: s};
+      setParsedStyle(temp);
+    };
+
+    if (!!styles) apply_resposive();
+  }, [state, styles]);
+
+  return {...state, styles: parsedStyle};
 };
